1a90e4504a81953f1da090a075ea6258b3121e76
Lina <lotosofhellforyou@gmail.com>
2023-04-30 12:59:11 +0300

    Added second projet files

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            int firstVal = 10;
+            int secondVal = 0;
+            int result = firstVal / secondVal;
+
+            // Этот оператор закомментирован
+            // ViewBag.Message = "Отладка приложения ASP.NET MVC!";
+
+            return View(result);
+        }
+    }
+}
